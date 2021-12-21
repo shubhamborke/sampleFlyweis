ae3338081aa4638c6f4e6fad6cd45a3ac327e1cf
shubhamborke <shubham.borke@gmail.com>
2021-12-21 19:46:16 +0530

    added

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.get("/home", (req,res) => {
+    res.send("hello")
+})
+
+app.get("/page1", (req,res) => {
+    res.send("page1")
+})
+
+app.get("/page2", (req,res) => {
+    res.send("page2")
+})
+
+app.get("/page3", (req,res) => {
+    res.send("page3")
+})
+
+app.get("/page4", (req,res) => {
+    res.send("page4")
+})
+
+app.listen(port, () => console.log("listing on port " + port))
